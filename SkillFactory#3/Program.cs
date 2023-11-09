bd5a4fe45d7e9aed65c9111e7678c252b12b2832
Илья Зуев <zueviliya777@gmail.com>
2023-11-10 01:19:38 +0300

    Задание 3.3.2

--- SkillFactory#3/Program.cs
+++ SkillFactory#3/Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет, \n мир");
-            Console.WriteLine('\x23');
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
         }
     }
 }
