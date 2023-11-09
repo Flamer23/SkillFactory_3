d79c0ba4b33ac9c4b6e5d49d21cc3e507e06bc2c
Илья Зуев <zueviliya777@gmail.com>
2023-11-10 01:14:24 +0300

    Изначальное состояние

--- SkillFactory#3/Program.cs
+++ SkillFactory#3/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string MyName;
+            MyName = "Jane";
+            Console.WriteLine(MyName);
         }
     }
 }
