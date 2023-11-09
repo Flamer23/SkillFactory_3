20b30358e6baf43e573e21235049b4690d09a38b
Илья Зуев <zueviliya777@gmail.com>
2023-11-10 01:18:36 +0300

    Задание 3.2.8

--- SkillFactory#3/Program.cs
+++ SkillFactory#3/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет, \nмир");
+            Console.WriteLine("Привет, \n мир");
+            Console.WriteLine('\x23');
         }
     }
 }
